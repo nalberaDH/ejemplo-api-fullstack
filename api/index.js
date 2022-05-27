53a7c3b4779d76d46a937bb5e8b2e4f83293504e
Nelson Albera <nalbera@hotmail.com>
2022-05-27 18:49:29 -0300

    actualizo api externa

--- api/index.js
+++ api/index.js
@@ -6,42 +6,32 @@ const axios = require('axios');
 const app = express();
 
 const apiInfo = async () => {
-    const allUsers = await axios('https://jsonplaceholder.typicode.com/users');
-    return json = allUsers.data.map((e)=> {
+    const allUsers = await axios('https://randomuser.me/api/');
+    const { results } = allUsers.data;
+    //console.log(results[0])
+    
+    return json = results.map((e) => {
         return{
-            id: e.id,
-            name: e.name,
-            userName: e.userName,
+            firstName: e.name.first,
+            lastName: e.name.last,
+            streetName: e.location.street.name,
+            streetNumber: e.location.street.number,
+            city: e.city,
+            country: e.country,
             email: e.email,
-            street: e.address.street,
-            city: e.address.city,
-            zipcode: e.address.zipcode,
             phone: e.phone,
-            website: e.website,
-            companyName: e.company.name,
+            picture: e.picture.large,
         }
-    });    
-}
+    })
+    
+    
+};
 
 app.get('/users', async (req,res)=>{
     
-    const name = req.query.name;
     const allUsers = await apiInfo();
-    if(!name){
-        return res.status(200).json(allUsers);
-    }
-    
-    const filterInfo = allUsers.filter((user) => user.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
-
-    filterInfo.length ? res.status(200).send(filterInfo) : res.sendStatus(404);
-    
+    res.status(200).json(allUsers);
 });
 
-app.get('/users/:id', async (req,res) => {
-    const { id } = req.params;
-    const allUsers = await apiInfo();
-    const user = allUsers.find((e) => e.id == id);console.log(user);
-    user ? res.status(200).json(user) : res.sendStatus(404);
-})
 
 app.listen(3001, () => console.log("Servidor escuchando en puerto 3001"))
